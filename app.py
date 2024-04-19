402bad283f44826f4d04b5a7e12eda755ba0ab24
TobiasFandrich <tobias.fandrich@marbet.com>
2024-04-19 11:46:56 +0200

    first run

--- app.py
+++ app.py
@@ -1,32 +1,61 @@
+from flask import Flask, render_template, request, jsonify
+import requests
+from dotenv import load_dotenv
 import os
 
-from flask import (Flask, redirect, render_template, request,
-                   send_from_directory, url_for)
-
+# Load environment variables from .env file
+load_dotenv()
+print("TOKEN_ENDPOINT:", os.getenv('TOKEN_ENDPOINT'))
+print("DATA_ENDPOINT:", os.getenv('DATA_ENDPOINT'))
+print("CLIENT_ID:", os.getenv('CLIENT_ID'))
+print("USERNAME:", os.getenv('USER'))
+print("PASSWORD:", os.getenv('PASSWORD'))
 app = Flask(__name__)
 
+# Retrieve the values from environment variables
+TOKEN_ENDPOINT = os.getenv('TOKEN_ENDPOINT')
+DATA_ENDPOINT = os.getenv('DATA_ENDPOINT')
+CLIENT_ID = os.getenv('CLIENT_ID')
+USERNAME = os.getenv('USER')
+PASSWORD = os.getenv('PASSWORD')
 
 @app.route('/')
 def index():
-   print('Request for index page received')
-   return render_template('index.html')
+    return render_template('form.html')
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+@app.route('/data/<string:event_code>/<string:participant_id>', methods=['GET'])
+def data(event_code, participant_id):
+    # Get the access token
+    token_response = requests.post(
+        TOKEN_ENDPOINT,
+        headers={
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        data={
+            'grant_type': 'password',
+            'username': USERNAME,
+            'password': PASSWORD,
+            'client_id': CLIENT_ID
+        }
+    )
+    token = token_response.json().get('access_token')
 
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
+    print(token)
 
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
 
+    # Use the token to get the data
+    data_response = requests.get(
+        f"{DATA_ENDPOINT}?eventCode={event_code}&filter={{\"participantIdOrUuid\": \"{participant_id}\"}}",
+        headers={
+            'Authorization': f'Bearer {token}',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Accept': 'application/json'
+        }
+    )
+    data = data_response.json()
+    print("API Response:", data)
+    # Render the data in the HTML table
+    return render_template('table.html', data=data)
 
 if __name__ == '__main__':
-   app.run()
+    app.run(debug=True)
